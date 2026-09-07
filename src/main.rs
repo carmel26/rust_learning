614f722de86db2b16b823f983491333452230d50
carmel26 <nkeshipelagie@gmail.com>
2026-09-07 19:50:28 +0300

    reference and pointer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
-    println!("Working with understanding reference!");  
-    let x: i32 = 25;
-    let r: &i32 = &x;
-    println!("The value of x is: {}", x);
-    println!("The value of r is: {}", r);
+    println!("Working with understanding reference!");   
+    let mut _x: i32 = 25;
+    let _r: &mut i32 = &mut _x;
+
+    *_r += 1; 
+    *_r -= 3;
+    
+    println!("The value of x is: {}", _x); 
+    // println!("The value of r is: {}", _r);
 }
