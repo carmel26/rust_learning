db6223eccd1adddbedbb04ff4727c14c6748ae92
carmel26 <nkeshipelagie@gmail.com>
2026-09-07 18:44:42 +0300

    working with function on 7Sept2026

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,35 @@
 fn main() {
-    // compound data types
-    // arrays, tuples, slices, structs, enums and Strings
-    
-    //  ===== arrays =====
-    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
-    println!("Th enumber in our array are: {:?}", numbers);
-    // print an error 
-    // let mix = [1, 2, 4, 'Apple', 3.13];
-    // println!("The mixed array is: {:?}", mix);
+    println!("Working with functions!");  
+    let sum = add(5, 10);
+    println!("Sum: {}", sum);
+    human_id("John Elan", 30, 175.9);
 
-    let fruits: [&str; 3] = ["Apple", "Banana", "Orange"];
-    println!("The fruits in our array are: {:?}", fruits);
-    println!("The first fruit in our array is: {}", fruits[0]);
+    // creating an expression which return the product of price and quantity
+    let x= {
+        let price = 5;
+        let quantity = 10;
+        price * quantity
+    };
+    println!("Total Price: {}", x);
 
-    // ===== tuples =====
-    let person: (String, i32, f64, bool) = ("John Paul".to_string(), 30, 164.9, true);
-    println!("The person is: {:?}", person);
-    let mixed_tuple = ("Kratos", 30, 164.9, true, numbers);
-    println!("The mixed tuple is: {:?}", mixed_tuple);
+    // calling the BMI function
+    let height_m = 1.75;  
+    let weight_kg = 70.0;  
+    let bmi = calculate_bmi(weight_kg, height_m);
+    println!("BMI: {:.2}", bmi);
+}
 
-    // ==== slices =====
-    let slice: &[i32] = &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    println!("the slice is equal to: {:?}", slice);
-    let animals: &[&str] = &["Dog", "Cat", "Bird", "Fish", "Rabbit"];
-    println!("the animals in our slice are: {:?}", animals);
 
-    // string vs Strings
-    let mut stone_cold: String = String::from("Hello, ");
-    stone_cold.push_str("Yeah!");
-    println!("Stone Cold says : {}",stone_cold);
 
-    // B-&str
-    let string : String = String::from("Bonjour tout le monde");
-    let slice: &str = &string;
-    println!("The slice is : {}", slice);
+// functions that return a value
+fn add(a: i32, b: i32) -> i32 {
+    return a + b;
+}
 
+fn human_id(name: &str, age: u32, height: f32){
+    println!("Name: {}, Age: {}, Height: {} cm", name, age, height);
+}
+
+fn calculate_bmi(weight_kg: f32, height_m: f32) -> f32 {
+    weight_kg / (height_m * height_m)
 }
