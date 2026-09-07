6de60883f95b3cfe99532304575a29030fb99804
carmel26 <nkeshipelagie@gmail.com>
2026-09-07 22:45:33 +0300

    working with immutable and mutable struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
 fn main() {
-    println!("Working with understanding reference!");   
-    let mut _x: i32 = 25;
-    let _r: &mut i32 = &mut _x;
+  // work with structs
+let mut account = BankAccount{
+    owner : "Alice".to_string(),
+    balance : 1200.45,
+};
+ 
+ // immutable account check balance
+account.check_balance();
 
-    *_r += 1; 
-    *_r -= 3;
-    
-    println!("The value of x is: {}", _x); 
-    // println!("The value of r is: {}", _r);
+// mutable borrow to withdraw money
+account.withdraw(230.1);
+
+// check balance again
+account.check_balance();
+
+}
+
+struct BankAccount {
+    owner : String,
+    balance : f64,
+}
+
+impl BankAccount{
+    fn withdraw(&mut self, amount: f64){
+        println!("Withdrawing {} from account owned by {}", amount, self.owner);
+        self.balance -= amount;
+    }
+
+    fn check_balance(&self){
+        println!("Account owned by  {} has a balance of {}", self.owner, self.balance);
+    }
 }
