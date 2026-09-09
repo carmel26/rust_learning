49257b788ff6ab1d8fbad62032a0ca0327da2472
carmel26 <nkeshipelagie@gmail.com>
2026-09-09 17:35:54 +0300

    working with ENUM type on 9Sept2026

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,37 @@
 #![allow(warnings)]
-// structures are used to name and package related values similar to tuples but with more flexibility
-  // creating a book structure
-  struct Book {
-    title: String,
-    author: String,
-    number_of_pages: u32,
-    available: bool,
-  }
 
-  // creating a user structure
-  struct User {
-    active : bool,
-    username : String,
-    email : String,
-    sign_in_count : u64,
-  }
+use std::net::IpAddr;
 
-fn main() {
-  // advanced structures understandings
-  let rect  = (30, 50);
-  
-  let mut user1 : User = User {
-    active : true,
-    username : String::from("carmel1"),
-    email : String::from("carmelnkeshi@gmail.com"),
-    sign_in_count : 1,
-  };
-
-//   changing the email of user1 using the object
-  user1.email = String::from("carmelnkeshi2@gmail.com");
-  println!("User email: {}", user1.email);
-  
-  let user2 : User = User {
-    email : String::from("mynewEmail@gmail.com"),
-    ..user1
-  };
-  println!("User email: {}", user2.email);
-
-  // creating a tuples struct
-  struct Color(i32, i32, i32);
-  struct Point(i32, i32);   
-
-  let black = Color(0, 0, 0);
-  let white = Color(255, 255, 255);
-
-  // unit-like struct
-  struct AlwaysEqual;
-  let subject: AlwaysEqual = AlwaysEqual;
-
-
+// enum type variables
+// definition of the enum type
+enum IpAddressKind {
+    V4(String), 
+    V6(String)
 }
 
-fn build_user(email: String, username:String) -> User {
-    User {
-      active : true,
-      username,
-      email,
-      sign_in_count : 1,
-    }
-  }
+struct ipAddress {
+    kind : IpAddressKind,
+    address : String,
+}
+fn main() {
+  // enum examples
+  let four : IpAddressKind = IpAddressKind::V4(String::from("127.0.0.1"));
+  let six : IpAddressKind = IpAddressKind::V6(String::from("::1"));
+
+  route(four);
+  route(six);
+
+  let home : ipAddress = ipAddress {
+    kind : IpAddressKind::V4(String::from("127.0.0.1")),
+    address : String::from("127.0.0.1"),
+  };
+
+  let loopBack : ipAddress = ipAddress {
+    kind : IpAddressKind::V6(String::from("::1")),
+    address : String::from("::1"),
+  };
+}
+
+fn route(ip_kind: IpAddressKind) {
+  // function body  
+}
