d98b24fd86265031b83d640bbbc5bfdd7dc4377a
carmel26 <nkeshipelagie@gmail.com>
2026-09-10 12:52:01 +0300

    working with vectors type with error hundling

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,34 @@
 #![allow(warnings)]
 
-// let's work with Option T
-// enum OPTION<T> {
-//   // Defining the generic option Type
-//   Some(T), // represents a value
-//   None, // represents the absence of a value
-// }
-
-// enum Result<T, E> {
-//   // Defining the generic result Type
-//   Ok(T), // represents a successful value
-//   Err(E), // represents an error value
-// }
+// working with vectors
 
 fn main() {
-    println!("Option hundling!");
-    let result = divide(10.0, 2.0);
-    match result {
-        Some(value) => println!("Result = {}", value),
-        None => println!("Error: Division by zero"),
+  // defining an empty vector
+   let mut vector: Vec<i32> = Vec::new();
+
+   vector.push(12);
+   vector.push(23);
+   vector.push(1);
+   vector.push(4);
+   vector.push(5);
+
+   println!("The first vector is {:?}", vector);
+
+   // defining a vector with values
+   let theVec : Vec<i32> = vec![12, 23, 34, 12, 32, 43, 32];
+
+   //by reference
+   let the_second_element : &i32 = &theVec[1];
+   println!("The second element of the second vector is {}", the_second_element);
+
+   // by value
+   let third_element = theVec[2];
+   println!("The third element of the second vector is {}", third_element);
+
+   //USING Get metho
+    match theVec.get(3) {
+        Some(third_element) => println!("The fourth element of the second vector is {}", third_element),
+        None => println!("There is no fourth element in the second vector"),
     }
 
-    println!("Result hundling!");
-    let result2: Result<f64, String> = second_divide(32.32, 10.02);
-    match result2 {
-        Ok(value) => println!("Second result = {}", value),
-        Err(error) => println!("Error: {}", error),
-    }
-}
-
-// using option type
-fn divide (numerator: f64, denominator: f64) ->Option<f64>{
-  if denominator == 0.0 {
-    None
-  } else {
-    Some(numerator / denominator)
-  }
-}
-
-fn second_divide(numerator: f64, denominator: f64) -> Result<f64, String> {
-    if denominator == 0.0 {
-        Err("Error: Division by zero".to_string())
-    } else {
-        Ok(numerator / denominator)
-    }
 }
